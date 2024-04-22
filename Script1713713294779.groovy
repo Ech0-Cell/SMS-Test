60396b84fc4814abd06685e6654659c6d671f022
SajedahGhizawi <166047790+SajedahGhizawi@users.noreply.github.com>
2024-04-22 09:52:00 +0300

    Add files via upload

---
+++ Script1713713294779.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.trendyol.com/giris?cb=%2F')
+
+WebUI.click(findTestObject('Object Repository/Page_/button_KABUL ET'))
+
+WebUI.setText(findTestObject('Object Repository/Page_/input_E-Posta_login email'), 'gsajedah@gmail.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_/input_ifre_login-password'), 'd8/GA+9vOZhdQnwZ0OPj4g==')
+
+WebUI.click(findTestObject('Object Repository/Page_/span_Giri Yap'))
+
+WebUI.closeBrowser()
+
